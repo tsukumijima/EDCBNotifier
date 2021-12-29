8b2c802a144070e64b68e301fd68725ec3c920b9
tsukumi <tsukumijima@users.noreply.github.com>
2021-12-30 02:10:27 +0900

    Fix: 処理の順番を並び替え

--- EDCBNotifier/EDCBNotifier.py
+++ EDCBNotifier/EDCBNotifier.py
@@ -77,7 +77,7 @@ def main():
         line = LINE(config.LINE_ACCESS_TOKEN)
 
         try:
-            result_line:dict = line.sendMessage(message, image=image)
+            result_line:dict = line.sendMessage(message, image_path=image)
         except Exception as error:
             print('[LINE Notify] Result: Failed')
             print('[LINE Notify] ' + colorama.Fore.RED + 'Error: ' + error.args[0], end='\n\n')
@@ -91,6 +91,24 @@ def main():
                 print('[LINE Notify] Result: Success (Code: ' + str(result_line['status']) + ')')
                 print('[LINE Notify] Message: ' + result_line['message'], end='\n\n')
 
+    # Discord にメッセージを送信
+    if ('Discord' in config.NOTIFY_TYPE):
+
+        discord = Discord(config.Discord_WEBHOOK_URL)
+
+        try:
+            result_discord:int = discord.sendMessage(message, image = image)
+        except Exception as error:
+            print('[Discord] Result: Failed')
+            print('[Discord] ' + colorama.Fore.RED + 'Error: ' + error.args[0], end = '\n\n')
+        else:
+            if result_discord != 204:
+                # ステータスが 204 以外（失敗）
+                print('[Discord] Result: Failed (Code: ' + str(result_discord) + ')', end = '\n\n')
+            else:
+                # ステータスが 200（成功）
+                print('[Discord] Result: Success (Code: ' + str(result_discord) + ')', end = '\n\n')
+
     # Twitter にツイートを送信
     if ('Tweet' in config.NOTIFY_TYPE):
 
@@ -103,7 +121,7 @@ def main():
 
         # ツイートを送信
         try:
-            result_tweet:dict = twitter.sendTweet(message, image=image)
+            result_tweet:dict = twitter.sendTweet(message, image_path=image)
         except Exception as error:
             print('[Tweet] Result: Failed')
             print('[Tweet] ' + colorama.Fore.RED + 'Error: ' + error.args[0], end='\n\n')
@@ -123,7 +141,7 @@ def main():
 
         # ダイレクトメッセージを送信
         try:
-            result_directmessage:dict = twitter.sendDirectMessage(message, image=image, destination=config.NOTIFY_DIRECTMESSAGE_TO)
+            result_directmessage:dict = twitter.sendDirectMessage(message, image_path=image, destination=config.NOTIFY_DIRECTMESSAGE_TO)
         except Exception as error:
             print('[DirectMessage] Result: Failed')
             print('[DirectMessage] ' + colorama.Fore.RED + 'Error: ' + error.args[0], end='\n\n')
@@ -132,24 +150,6 @@ def main():
             print('[DirectMessage] Message: https://twitter.com/messages/' +
                   result_directmessage['event']['message_create']['target']['recipient_id'] + '-' +
                   result_directmessage['event']['message_create']['sender_id'], end='\n\n')
-
-    # Discord にメッセージを送信
-    if ('Discord' in config.NOTIFY_TYPE):
-
-        discord = Discord(config.Discord_WEBHOOK_URL)
-
-        try:
-            result_discord:int = discord.sendMessage(message, image = image)
-        except Exception as error:
-            print('[Discord] Result: Failed')
-            print('[Discord] ' + colorama.Fore.RED + 'Error: ' + error.args[0], end = '\n\n')
-        else:
-            if result_discord != 204:
-                # ステータスが 204 以外（失敗）
-                print('[Discord] Result: Failed (Code: ' + str(result_discord) + ')', end = '\n\n')
-            else:
-                # ステータスが 200（成功）
-                print('[Discord] Result: Success (Code: ' + str(result_discord) + ')', end = '\n\n')
 
 
 if __name__ == '__main__':
